a757aa387eb5df10a05aa356a34371b75fbe6b86
[Julmar Figueroa] <[julmarfigueroa@gmail.com]>
2016-03-28 21:17:44 -0400

    First commit

---
+++ Exercise_1.js
@@ -0,0 +1,11 @@
+// Traversing an Array
+// Exercise 1: Print all the names in an array
+
+function test(){
+  var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+  for(var i = 0; i < names.length; i++){
+    console.log(names[i]);
+  }
+}
+test();
---
+++ Exercise_2.js
@@ -0,0 +1,10 @@
+// Exercise 2: Use forEach to print all the names in an array
+
+function test(){
+  var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+  names.forEach(function(name){
+      console.log(name);
+  });
+}
+test();
